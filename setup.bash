21d3badc3f30c95b4c21d1430b78add9cf47ab5f
☃, the Unicode Snowman <oconnor.57@wright.edu>
2019-03-19 09:26:07 -0400

    Add database selection to start script

--- setup.bash
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
- 
- # Change to the app source directory
- popd src
- 
- npm install
--- start.bash
+++ start.bash
@@ -1,26 +1,36 @@
- #!/bin/bash
- 
- # Change to the app source directory
- pushd src
- 
- # Install app dependencies
- npm install
- 
- # Start the app as a background process
- npm start &>>../app.log &
- 
- # Go back to the revious directory
- popd
- 
- # Change to the server directory
- pushd server
- 
- # Install its dependencies
- npm install
- 
- # Backto root directory...
- popd
- 
- # Start the other part of the server
- npm run start-server &>>server.log &
- 
+#!/bin/bash
+
+# Change to the app source directory
+pushd src
+
+# Install app dependencies
+npm install
+
+# Start the app as a background process
+npm start &>>../app.log &
+
+# Go back to the revious directory
+popd
+
+# Change to the server directory
+pushd server
+
+# Install its dependencies
+npm install
+
+# Backto root directory...
+popd
+
+# Start the other part of the server
+
+echo -ne "Would you like to use the \033[4ma\033[0mws or \033[4mt\033[0mabletop database? "
+
+read usrin
+
+if [[ $usrin =~ ^[Aa] ]]; then
+  database="aws"
+else
+  database="tabletop"
+fi
+
+npm run start-server $database &>>server.log &
