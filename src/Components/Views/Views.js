0e0e62b1de393e2c964a616d16bd1fb38407b1bf
Jared <jared.cole@outlook.com>
2019-03-20 23:50:43 -0400

    Fixed json package format issues
    removing swipe nav between pages

--- src/Components/Views/Views.js
+++ src/Components/Views/Views.js
@@ -4,20 +4,17 @@ import FAQ from './FAQ';
 import About from './About';
 import './Views.css';
 
-import SwipableRoutes from 'react-swipeable-routes';
-import {HashRouter, Route} from 'react-router-dom';
+import {HashRouter, Route, Redirect} from 'react-router-dom';
 
 
 const Views = props =>{
     return(
       <div className={'myDiv'}>
-          <HashRouter>
-          <SwipableRoutes>
+            
+                <Redirect to="Map" />
               <Route exact path="Map" component={Map}/>
               <Route exact path='About' component ={About}/>
               <Route exact path="FAQ" component={FAQ}/>
-          </SwipableRoutes>
-          </HashRouter>
       </div>
     );
 
