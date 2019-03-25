ce51c591b3ed04a9013719ea9181083342337cb5
Eric Foy <foy.8@wright.edu>
2019-03-25 17:58:33 -0400

    regex url path match

--- server/src/server.js
+++ server/src/server.js
@@ -5,12 +5,9 @@ const pathfinder = require('./pathfinder.js');
 
 const server = http.createServer(async function (req, res) {
     const parsedUrl = url.parse(req.url, true);
-    if (parsedUrl.pathname === '/getPath' && parsedUrl.query.start && parsedUrl.query.end) {
-        const path = await pathfinder.getPath(+parsedUrl.query.start, +parsedUrl.query.end);
-        res.end(JSON.stringify(path));
-    } else {
-        res.end();
-    }
+    if (!parsedUrl.path.match(/getPath\?start=\d+&end=\d+/)) res.end();
+    const path = await pathfinder.getPath(+parsedUrl.query.start, +parsedUrl.query.end);
+    res.end(JSON.stringify(path));
 });
 
 server.listen(5000);
