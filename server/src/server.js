debeb3ff5dbb68f21a86bb2682832e557fa2d7c6
Eric Foy <foy.8@wright.edu>
2019-03-25 16:33:45 -0400

    assert server path

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const querystring = require('querystring');
+const url = require('url');
 const pathfinder = require('./pathfinder.js');
 
 const server = http.createServer(async function (req, res) {
-    const query = querystring.parse(req.url.replace(/^\/[a-zA-Z0-9]*\?/,''));
-    res.end(JSON.stringify(await pathfinder.getPath(+query.start, +query.end)));
+    const parsedUrl = url.parse(req.url, true);
+    if (parsedUrl.pathname === '/getPath' && parsedUrl.query.start && parsedUrl.query.end) {
+        const path = await pathfinder.getPath(+parsedUrl.query.start, +parsedUrl.query.end);
+        res.end(JSON.stringify(path));
+    } else {
+        res.end();
+    }
 });
 
 server.listen(5000);
