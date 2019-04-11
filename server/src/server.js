3ffa89fb6d6d584dbd1dd0a70a81ea854222dd3d
Eric Foy <foy.8@wright.edu>
2019-04-11 11:46:31 -0400

    server log from react post

--- server/src/server.js
+++ server/src/server.js
@@ -7,21 +7,39 @@ server.listen(5000);
 server.setTimeout();
 
 server.on("request", async function(request, response) {
-  const parsedUrl = parseUrl(request.url);
-  if (checkUrl(parsedUrl)) {
-    const path = await preparePath(parsedUrl);
-    response.end(JSON.stringify(path));
-  }
+    switch (request.method) {
+        case "GET":
+            const parsedUrl = parseUrl(request.url);
+            if (checkUrl(parsedUrl)) {
+                const path = await preparePath(parsedUrl);
+                response.end(JSON.stringify(path));
+            }
+            break;
+        case "POST":
+            let body = "";
+            request.on("data", function(chunk) {
+                body += chunk;
+            });
+            request.on("end", function() {
+                console.log(body);
+                response.end(body);
+            });
+            break;
+    }
+});
+
+server.on("data", function(chunk) {
+    console.log(chunk);
 });
 
 function checkUrl(parsedUrl) {
-  return parsedUrl.path.match(/getPath\?start=\d+&end=\d+/);
+    return parsedUrl.path.match(/getPath\?start=\d+&end=\d+/);
 }
 
 function parseUrl(requestUrl) {
-  return url.parse(requestUrl, true);
+    return url.parse(requestUrl, true);
 }
 
 async function preparePath(parsedUrl) {
-  return pathfinder.getPath(+parsedUrl.query.start, +parsedUrl.query.end);
+    return pathfinder.getPath(+parsedUrl.query.start, +parsedUrl.query.end);
 }
