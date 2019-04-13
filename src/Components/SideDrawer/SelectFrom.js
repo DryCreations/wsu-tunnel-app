2c1e3b210e8d21f93c3652fcf741b121d765c086
Your Name <eilerman.42@wright.edu>
2019-04-13 01:29:18 -0400

    Changed side drawer elements

--- src/Components/SideDrawer/SelectFrom.js
+++ src/Components/SideDrawer/SelectFrom.js
@@ -2,19 +2,37 @@ import React from "react";
 import Dropdown from "react-dropdown";
 import "./SelectFrom.css";
 
-const SelectFrom = props => {
-  const options = ["A", "B", "C", "D", "E"];
-  const defaultOption = options[0];
+// const SelectFrom = props => {
+//   const options = ["A", "B", "C", "D", "E"];
+//   const defaultOption = options[0];
 
-  return (
-    <div>
-      <p>Select Starting Point:</p>
-      <Dropdown
-        options={options}
-        value={defaultOption}
-        placeholder="Select an option"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <p>Select Starting Point:</p>
+//       <Dropdown
+//         options={options}
+//         value={defaultOption}
+//         placeholder="Select an option"
+//       />
+//     </div>
+//   );
+// };
+
+class SelectFrom extends React.Component {
+    render() {
+      return(
+          <div id="select-from">
+            <p>Select starting point:</p>
+            <select>
+                <option value="Russ">Russ Engineering Center</option>
+                <option value="Allyn">Allyn Hall</option>
+                <option value="Millett">Millett Hall</option>
+                <option value="Fawcet">Fawcet Hall</option>
+                <option value="Oelman">Oelman Hall</option>
+            </select>
+          </div>
+      )
+    }
+  }
+
 export default SelectFrom;
--- src/Components/SideDrawer/SelectTo.js
+++ src/Components/SideDrawer/SelectTo.js
@@ -2,19 +2,37 @@ import React from "react";
 import Dropdown from "react-dropdown";
 import "./SelectTo.css";
 
-const SelectTo = props => {
-  const options = ["A", "B", "C", "D", "E"];
-  const defaultOption = options[0];
+// const SelectTo = props => {
+//   const options = ["A", "B", "C", "D", "E"];
+//   const defaultOption = options[0];
 
-  return (
-    <div>
-      <p>Select Destination</p>
-      <Dropdown
-        options={options}
-        value={defaultOption}
-        placeholder="Select an option"
-      />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <p>Select Destination</p>
+//       <Dropdown
+//         options={options}
+//         value={defaultOption}
+//         placeholder="Select an option"
+//       />
+//     </div>
+//   );
+// };
+
+class SelectTo extends React.Component {
+  render() {
+    return(
+        <div id="select-to">
+            <p>Select destination:</p>
+            <select>
+                <option value="Russ">Russ Engineering Center</option>
+                <option value="Allyn">Allyn Hall</option>
+                <option value="Millett">Millett Hall</option>
+                <option value="Fawcet">Fawcet Hall</option>
+                <option value="Oelman">Oelman Hall</option>
+            </select>
+        </div>
+    )
+  }
+}
+
 export default SelectTo;
