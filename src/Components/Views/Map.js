221b4a7dba1e56406e5f9e9e76bbf4437653a3a3
Eric Foy <foy.8@wright.edu>
2019-04-17 15:07:02 -0400

    padding to help

--- src/Components/Views/Map.js
+++ src/Components/Views/Map.js
@@ -144,7 +144,7 @@ class Map extends Component {
 
           <img id="Compass" src="north.png" alt="compass" />
         </div>
-        <div style={{display: this.state.displayHelp, overflow: 'auto'}}>
+        <div style={{display: this.state.displayHelp, overflow: 'auto', padding: '65px 0px 65px 0px'}}>
           <HelpPage />
         </div>
         <MyFooter />
