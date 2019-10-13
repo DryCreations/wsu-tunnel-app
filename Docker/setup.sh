999364a5db6a0bbccb4d0fee7fb5ed97edbf61ae
☃, the Unicode Snowman <oconnor.57@wright.edu>
2019-10-13 12:16:00 -0400

    IT LIVES!!!!!!
    *evil cackle*

--- Docker/setup.sh
+++ Docker/setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# I don't really understand why this works, but it does, so I'm keeping it
-npm install -g react-scripts
-npm install -g react-scripts
-
 # Get the mysql repository
 wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.13-1_all.deb
 echo "About to setup the mysql repository. Please select 'MySQL Server & Cluster'" \
-    "for the product to configure and mysql-5.7 for the version"
+    "for the product to configure and mysql-8.0 for the version"
 dpkg -i mysql-apt-config_0.8.13-1_all.deb
 rm mysql-apt-config_0.8.13-1_all.deb
 
@@ -20,9 +16,6 @@ apt install -y mysql-community-server
 
 # Start mysql. Again, I have no clue why this works, but it does
 runuser -l mysql -s /bin/bash -c '/usr/sbin/mysqld --defaults-file=/etc/mysql/my.cnf --basedir=/usr --datadir=/var/lib/mysql --pid-file=/var/run/mysqld/mysqld.pid --socket=/var/run/mysqld/mysqld.sock' &
-
-# Download the course repo
-git clone https://github.com/RLey/wsu-tunnel-app.git /root/repo
 
 echo "What branch would you like to use to run the server?"
 read branch
@@ -37,12 +30,8 @@ pushd /root/repo
 
 git checkout $branch
 
-# et the path for the database that the user wants to use
-echo -ne "Please enter the path of the .sql file to use\072 "
-read sqlfile
-
 # If no database was specified, use a default
-sqlfile=${sqlfile:-'/root/repo/Docker/databaseData.sql'}
+sqlfile='/root/databaseData.sql'
 
 echo "Using sql file $sqlfile"
 
@@ -50,7 +39,7 @@ echo "Using sql file $sqlfile"
 if [ -e $sqlfile ]; then
     mysql -p < $sqlfile
 else
-    mysql -p < /root/repo/Docker/databaseData.sql
+    mysql -p < /root/databaseData.sql
 fi
 
 # Create a non-admin user to access the database
@@ -59,30 +48,8 @@ echo "GRANT SELECT ON wsutunnelapp.* TO 'tunnelapp-user'@'localhost';" >> makeUs
 mysql -p < makeUser
 rm makeUser
 
-# Change to the app source directory
-pushd src
-
-# Install app dependencies
-npm install
-npm install
 # Start the app as a background process on port 80
 npm run start80 &>>app.log &
-portNumber=80
-
-# Go back to the revious directory
-popd
-
-# Change to the server directory
-pushd server
-
-# Install its dependencies
-npm install
-npm install
-
-# Backto root directory...
-popd
 
 # Start the other part of the server
 npm run start-server-local &>>server.log &
-
-popd
